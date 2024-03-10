7fc166af894fd045cd0802075ed21776f6e3f7cd
maysaraujo <maysaaraujo25@gmail.com>
2024-03-09 21:26:40 -0300

    feat: adding footer in App.jsx

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer>
+      Challenge by{' '}
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+        Frontend Mentor
+      </a>
+      . Coded by <a href='https://github.com/maysaraujo'>Maysa Araujo</a>.
+    </footer>
+  );
+};
+
+export default Footer;
